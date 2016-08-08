380c92e8311e0f43dcf95f71b245a3b41401fe33
hadley <h.wickham@gmail.com>
2016-08-08 13:33:33 -0500

    Add re-leveling

---
+++ R/relevel.R
@@ -0,0 +1,34 @@
+#' Change the levels of a factor
+#'
+#' @param x A factor
+#' @param ... A sequence of named character vectors where the
+#'   name gives the new level, and the value gives the old level.
+#'   Levels not otherwise mentioned will be left as is.
+#' @export
+#' @examples
+#' x <- factor(c("apple", "bear", "banana", "dear"))
+#' fct_relevel(x, fruit = "apple", fruit = "banana")
+#'
+#' # If you make a mistake you'll get a warning
+#' fct_relevel(x, fruit = "apple", fruit = "bananana")
+fct_relevel <- function(x, ...) {
+  new_levels <- c(...)
+
+  # Match old levels with new levels
+  old_levels <- levels(x)
+  idx <- match(new_levels, old_levels)
+
+  # Handle levels that don't exist
+  if (any(is.na(idx))) {
+    bad <- new_levels[is.na(idx)]
+    warning("Unknown levels in `x`: ", paste(bad, collapse = ", "), call. = FALSE)
+
+    new_levels <- new_levels[!is.na(idx)]
+    idx <- idx[!is.na(idx)]
+  }
+
+  old_levels[idx] <- names(new_levels)
+  levels(x) <- old_levels
+
+  x
+}
