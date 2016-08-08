ec029699782a72b661cf7f9181bcedd0b0580ef2
hadley <h.wickham@gmail.com>
2016-08-08 14:32:21 -0500

    First pass at infreq()

--- R/fct_inorder.R
+++ R/fct_inorder.R
@@ -1,11 +1,20 @@
-#' Reorders levels in order of first appearance in data
+#' Reorders levels in order of first appearance or frequency.
 #'
 #' @param f As factor
 #' @export
 #' @examples
-#' f <- factor(c("b", "b", "a", "a", "c", "c"))
+#' f <- factor(c("b", "b", "a", "c", "c", "c"))
 #' f
 #' fct_inorder(f)
+#' fct_infreq(f)
 fct_inorder <- function(f) {
   factor(f, levels = levels(f)[as.integer(f[!duplicated(f)])])
+}
+
+#' @export
+#' @rdname fct_inorder
+fct_infreq <- function(f) {
+  count <- fct_count(f)
+
+  factor(f, levels = count$level[order(count$n, decreasing = TRUE)])
 }
