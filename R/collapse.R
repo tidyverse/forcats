1e549db8fc6ce594d6fbe35724d1de430ba41833
hadley <h.wickham@gmail.com>
2016-08-08 13:25:52 -0500

    Specify either n or prop

--- R/collapse.R
+++ R/collapse.R
@@ -1,20 +1,34 @@
 #' Collapse rare levels into "other".
 #'
 #' @param x A factor.
-#' @param threshold Values with less than this proportion of entries will
-#'   be collpased
+#' @param n,prop Either specify \code{n} or \code{prop}.
+#'
+#'   If you specify \code{n}, only the most common \code{n} values will be kept
+#'   (unless there are ties, in which case it will keep more).
+#'
+#'   If you specify \code{prop}, only values that appear at least that
+#'   commonly will be kept.
 #' @param other_level Value of level used for "other" values.
 #' @export
 #' @examples
 #' x <- factor(letters[rpois(100, 5)])
 #' x
-#' fct_collapse(x)
-fct_collapse <- function(x, threshold = 0.1, other_level = "Other") {
+#' fct_collapse(x, n = 3)
+#' fct_collapse(x, prop = 0.1)
+fct_collapse <- function(x, n, prop, other_level = "Other") {
 
   count <- fct_count(x)
-  count$prop <- count$n / sum(count$n)
 
-  new_levels <- ifelse(count$prop > threshold, count$level, other_level)
+  if (!xor(missing(n), missing(prop))) {
+    stop("You must specify one of `n` or `prop`", call = FALSE)
+  } else if (!missing(n)) {
+    rank <- rank(-count$n, ties = "first")
+    new_levels <- ifelse(rank <= n, count$level, other_level)
+  } else if (!missing(prop)) {
+    count$prop <- count$n / sum(count$n)
+    new_levels <- ifelse(count$prop > prop, count$level, other_level)
+  }
+
   levels(x) <- new_levels
   x
 }
