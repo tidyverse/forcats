3a1b59514baff0c90905f12b5d60b5ef80262bc9
hadley <h.wickham@gmail.com>
2016-08-08 14:02:50 -0500

    First pass at fct_reorder2

--- R/reorder.R
+++ R/reorder.R
@@ -7,9 +7,11 @@
 #' \code{fct_reorder} is currently an almost exact copy of
 #' \code{\link[stats]{reorder}} but it may change in the future.
 #'
-#' @param x A factor
-#' @param y,fun fun The levels of \code{x} will be reordered so that \code{fun}
+#' @param f A factor
+#' @param x,y fun The levels of \code{f} will be reordered so that \code{fun}
 #'    apply to each group is in ascending order.
+#' @param fun An summary function. It should take one vector for
+#'   \code{fct_reorder}, and two vectors for \code{fct_reorder2}.
 #' @param ... Other arguments passed on to \code{fun}. A common argument is
 #'   \code{na.rm = TRUE}.
 #' @importFrom stats median
@@ -17,12 +19,41 @@
 #' @examples
 #' boxplot(Sepal.Width ~ Species, data = iris)
 #' boxplot(Sepal.Width ~ fct_reorder(Species, Sepal.Width), data = iris)
-fct_reorder <- function(x, y, fun = median, ...) {
+#'
+#' chks <- subset(ChickWeight, as.integer(Chick) < 20)
+#'
+#' if (require("ggplot2")) {
+#' ggplot(chks, aes(Time, weight, colour = Chick)) +
+#'   geom_point() +
+#'   geom_line()
+#' ggplot(chks, aes(Time, weight, colour = fct_reorder2(Chick, Time, weight))) +
+#'   geom_point() +
+#'   geom_line()
+#' }
+fct_reorder <- function(f, x, fun = median, ...) {
+  stopifnot(length(f) == length(x))
 
-  summary <- tapply(y, x, fun, ...)
-  if (!is.numeric(summary) || length(summary) != nlevels(x)) {
+  summary <- tapply(x, factor(f), fun, ...)
+  if (!is.numeric(summary)) {
     stop("`fun` must return a single number per group", call. = FALSE)
   }
 
-  factor(x, levels = levels(x)[order(summary)])
+  factor(f, levels = levels(f)[order(summary)])
+}
+
+#' @export
+#' @rdname fct_reorder
+fct_reorder2 <- function(f, x, y, fun = last2, ...) {
+  stopifnot(length(f) == length(x), length(x) == length(y))
+
+  summary <- tapply(seq_along(x), factor(f), function(i) fun(x[i], y[i], ...))
+  if (!is.numeric(summary)) {
+    stop("`fun` must return a single number per group", call. = FALSE)
+  }
+
+  factor(f, levels = levels(f)[order(summary)])
+}
+
+last2 <- function(x, y) {
+  y[order(x, na.last = FALSE)][length(y)]
 }
