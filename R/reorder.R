c65d2885f6c2cdc3f058e46c99a8963bd20db11b
hadley <h.wickham@gmail.com>
2016-08-09 07:45:30 -0500

    Move all reordering methods to one file

--- R/reorder.R
+++ R/reorder.R
@@ -59,3 +59,28 @@ fct_reorder2 <- function(f, x, y, fun = last2, ...) {
 last2 <- function(x, y) {
   y[order(x, na.last = FALSE)][length(y)]
 }
+
+
+#' Reorders levels in order of first appearance or frequency.
+#'
+#' @param f A factor
+#' @export
+#' @examples
+#' f <- factor(c("b", "b", "a", "c", "c", "c"))
+#' f
+#' fct_inorder(f)
+#' fct_infreq(f)
+fct_inorder <- function(f) {
+  f <- check_factor(f)
+
+  factor(f, levels = levels(f)[as.integer(f[!duplicated(f)])])
+}
+
+#' @export
+#' @rdname fct_inorder
+fct_infreq <- function(f) {
+  f <- check_factor(f)
+  count <- fct_count(f)
+
+  factor(f, levels = count$level[order(count$n, decreasing = TRUE)])
+}
