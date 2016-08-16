226057042bcb0b6e1a40aeb8b29ee24d081175ef
hadley <h.wickham@gmail.com>
2016-08-16 16:07:07 -0500

    Add sample data from the GSS

---
+++ R/data.R
@@ -0,0 +1,19 @@
+#' A sample of categorical variables from the General Social survey
+#'
+#' @source Downloaded from \url{https://gssdataexplorer.norc.org/}.
+#' @format
+#' \describe{
+#'  \item{year}{year of survey, 2000--2014}
+#'  \item{marital}{marital status}
+#'  \item{race}{race}
+#'  \item{rincome}{reported income}
+#'  \item{partyid}{party affiliation}
+#'  \item{relig}{religion}
+#'  \item{denom}{denomination}
+#' }
+#' @examples
+#' gss_cat
+#'
+#' fct_count(gss_cat$relig)
+#' fct_count(fct_lump(gss_cat$relig))
+"gss_cat"
