b64476f1ed91da957e67cd39c30b189da948b63f
jennybc <jenny@stat.ubc.ca>
2016-09-07 08:40:59 -0700

    Add package-level help page

---
+++ R/forcats-package.r
@@ -0,0 +1,10 @@
+#' forcats.
+#'
+#' Helpers for reordering factor levels (including moving specified levels to
+#' front, ordering by first appearance, reversing, and randomly shuffling), and
+#' tools for modifying factor levels (including collapsing rare levels into
+#' other, anonymising, and manually recoding).
+#'
+#' @name forcats
+#' @docType package
+NULL
