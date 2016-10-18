1eb4fb0d0e5f045e3a7562ab0b4526f71a859da9
Marcus Walz <marcusevanwalz@gmail.com>
2016-10-18 14:10:08 -0500

    Update paste.R
    `check_factor` applied to input.

--- R/paste.R
+++ R/paste.R
@@ -1,9 +1,10 @@
-#' Paste operation for factors while preserving sequence
-#' via cartesian product
+#' "Paste" together factors.
+#' 
+#' Order is cartesian product of all levels.
 #' 
 #' This is sometimes useful when plotting a factor
 #'
-#' @param ... Factors to be united
+#' @param ... Factors to be united. 
 #' @param sep Separtor to use
 #' @export
 #' @examples
@@ -11,13 +12,13 @@
 #' g <- factor(c("x", "y", "z"))
 #' fct_paste(f, g, sep="-")
 fct_paste <- function(..., sep=" ") {
-  Map(check_factor, list(...))
+  input_fct <- Map(check_factor, list(...))
   
-  my_paste <- function(...) do.call(paste, append(list(...), list(sep=sep)))
+  my_paste <- function(input) do.call(paste, append(input, list(sep=sep)))
    
-  fcts <- Map(function(fct) as.character(levels(fct)),list(...))
+  fcts <- Map(function(fct) as.character(levels(fct)),input_fct)
   
   new_factor_lvls <- do.call( my_paste , do.call(expand.grid, fcts) )
   
-  factor(my_paste(...), levels=new_factor_lvls)
+  factor(my_paste(input_fct), levels=new_factor_lvls)
 }
