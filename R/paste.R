c36f91ab2a8b0df4f385da79ea176eb1967358d0
Walz <Marcus.Walz@aurora.org>
2016-10-18 14:51:31 -0500

    All working

--- R/paste.R
+++ R/paste.R
@@ -1,10 +1,10 @@
 #' "Paste" together factors.
-#' 
+#'
 #' Order is cartesian product of all levels.
-#' 
+#'
 #' This is sometimes useful when plotting a factor
 #'
-#' @param ... Factors to be united. 
+#' @param ... Factors to be united.
 #' @param sep Separtor to use
 #' @export
 #' @examples
@@ -13,12 +13,13 @@
 #' fct_paste(f, g, sep="-")
 fct_paste <- function(..., sep=" ") {
   input_fct <- Map(check_factor, list(...))
-  
-  my_paste <- function(input) do.call(paste, append(input, list(sep=sep)))
-   
-  fcts <- Map(function(fct) as.character(levels(fct)),input_fct)
-  
-  new_factor_lvls <- do.call( my_paste , do.call(expand.grid, fcts) )
-  
-  factor(my_paste(input_fct), levels=new_factor_lvls)
+
+  my_paste <- function(...) do.call(paste, append(list(...), list(sep=sep)))
+
+  fcts <- Map(function(fct) as.character(levels(fct)), input_fct)
+
+  new_factor_lvls <- do.call( my_paste , do.call( expand.grid, fcts ) )
+
+  print(new_factor_lvls)
+  factor(do.call(my_paste, input_fct), levels=new_factor_lvls)
 }
