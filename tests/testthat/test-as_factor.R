59cbe63ebddfe1197d5d63159c46144f87cd9a08
Kirill Müller <krlmlr@users.noreply.github.com>
2018-02-10 17:11:57 +0100

    Allow NA in fct_inorder() (#98)

--- tests/testthat/test-as_factor.R
+++ tests/testthat/test-as_factor.R
@@ -4,3 +4,8 @@ test_that("equivalent to fct_inorder", {
   x <- c("a", "z", "g")
   expect_equal(as_factor(x), fct_inorder(x))
 })
+
+test_that("supports NA (#89)", {
+  x <- c("a", "z", "g", NA)
+  expect_equal(as_factor(x), fct_inorder(x))
+})
