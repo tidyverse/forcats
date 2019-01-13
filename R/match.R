694f9ccc14ca39b991a1b192b67af74d03867b54
Jonathan Carroll <jono@jcarroll.com.au>
2019-01-13 16:28:34 +1030

    address review notes

--- R/match.R
+++ R/match.R
@@ -7,26 +7,20 @@
 #' @md
 #' @rdname fct_match
 #' @param f A factor (or character vector).
-#' @param inverse (logical) if `TRUE`, perform a "not match" operation.
+#' @param lvls A vector of characters for which the test "are these levels in
+#'   `f`?" should be performed.
 #' @param allow_missing (logical) if `TRUE`, allow matching to empty levels in
 #'   f
-#' @param ... A series of characters for which the test "are these levels in
-#'   `f`?" should be performed.
-#'
-#' @return a logical vector determined by whether or not the levels in `...` are
-#'   present in `f`
 #'
 #' @details Unlike `%in%`, which performs a basic yes/no test, this function
 #'   performs validation on the input vector to ensure that the levels tested
 #'   for are all present before creating a logical vector. If a tested level is
-#'   not present, then an error is thrown rather than producing  a silent
+#'   not present, then an error is thrown rather than producing a silent
 #'   `FALSE`.
 #'
 #'   Since a distinction can be made between `levels(f)` and `unique(f)` (the
 #'   latter excludes empty levels) the argument `allow_missing` can be
 #'   specified.
-#'
-#'   `fct_exclude` is a shortcut to `fct_match(inverse = TRUE)`.
 #'
 #'   This is intended to replace the common pattern
 #'
@@ -38,47 +32,33 @@
 #'
 #'   where an error will be generated if `"x"` or `"y"` are not levels in `d$l`.
 #'
+#' @return A logical vector
+#'
 #' @export
-#' @importFrom rlang dots_list
 #'
 #' @examples
-#' ## TRUE if present
-#' gss_cat$marital %>% fct_match("Married", "Divorced")
-#' ## TRUE if not present (exclude)
-#' gss_cat$marital %>% fct_match("No answer", inverse = TRUE)
-#' gss_cat$marital %>% fct_exclude("No answer")
-#' ## importantly, misspelled levels throw an error
-#' \dontrun{gss_cat$marital %>% fct_match("Married", "Davorced")}
-#' ## match to levels()
-#' gss_cat$race %>% fct_match("Other", "Not applicable")
-#' ## match to unique()
-#' gss_cat$race %>% fct_match("Other", "Not applicable", allow_missing = TRUE)
-fct_match <- function(f, ..., inverse = FALSE, allow_missing = TRUE) {
+#' gss_cat$marital %>% fct_match(c("Married", "Divorced"))
+#' # importantly, misspelled levels throw an error
+#' \dontrun{gss_cat$marital %>% fct_match(c("Married", "Davorced"))}
+#' # match to levels()
+#' gss_cat$race %>% fct_match(c("Other", "Not applicable"))
+#' # match to unique()
+#' gss_cat$race %>% fct_match(c("Other", "Not applicable"), allow_missing = TRUE)
+fct_match <- function(f, lvls, allow_missing = TRUE) {
   f <- check_factor(f)
 
-  stopifnot(is.logical(inverse))
   stopifnot(is.logical(allow_missing))
 
-  dots <- rlang::dots_list(...)
-  lvls <- unlist(dots, use.names = FALSE)
   if (allow_missing) {
     all <- levels(f)
   } else {
     all <- unique(f)
   }
+
   validate_level_inputs(all, lvls)
-  ans <- f %in% lvls
 
-  if (inverse) ans <- !ans
+  return(f %in% lvls)
 
-  ans
-}
-
-
-#' @rdname fct_match
-#' @export
-fct_exclude <- function(f, ..., allow_missing = TRUE) {
-  fct_match(f, ..., inverse = TRUE, allow_missing = allow_missing)
 }
 
 
@@ -87,15 +67,15 @@ fct_exclude <- function(f, ..., allow_missing = TRUE) {
 #' @noRd
 validate_level_inputs <- function(factor, check) {
   levels_present <- check %in% factor
-  if (!all(levels_present)) {
-    stop(
-      paste0(
-        "Level(s) not present in factor: \"",
-        toString(check[which(!levels_present)]),
-        "\""
-      ),
-      call. = FALSE
-    )
+  if (all(levels_present)) {
+    return(NULL)
   }
-  NULL
+  stop(
+    paste0(
+      "Level(s) not present in factor: \"",
+      encodeString(check[which(!levels_present)]),
+      "\""
+    ),
+    call. = FALSE
+  )
 }
