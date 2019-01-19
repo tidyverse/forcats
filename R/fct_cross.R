f0a853cedc75e327f27e2397de1b707f2ce53377
Thomas Lumley <t.lumley@auckland.ac.nz>
2019-01-19 14:31:05 -0600

    use list2(...) instead of list()

--- R/fct_cross.R
+++ R/fct_cross.R
@@ -19,7 +19,7 @@
 fct_cross <- function(.f, ..., sep = ":", keep_empty = FALSE) {
   .f <- check_factor(.f)
 
-  flist <- list(...)
+  flist <- list2(...)
   if (length(flist) == 0) {
     return(.f)
   }
