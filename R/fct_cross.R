b6579744d7af3df5355d95f43d3970d05c86675b
Scott Brenstuhl <brenstsr@miamioh.edu>
2020-01-31 13:34:06 -0800

    Replace rlang::invoke with rlang::exec in fct_cross (#243)
    Fixes #204

--- R/fct_cross.R
+++ R/fct_cross.R
@@ -24,11 +24,12 @@ fct_cross <- function(..., sep = ":", keep_empty = FALSE) {
 
   .data <- tibble::as_tibble(flist, .name_repair = "minimal")
   .data <- lapply(.data, check_factor)
-  newf <- invoke(paste, .data, sep = sep)
+
+  newf <- exec(paste, !!!.data, sep = sep)
 
   old_levels <- lapply(.data, levels)
-  grid <- invoke(expand.grid, old_levels)
-  new_levels <- invoke(paste, grid, sep = sep)
+  grid <- exec(expand.grid, old_levels)
+  new_levels <- exec(paste, !!!grid, sep = sep)
 
   if (!keep_empty) {
     new_levels <- intersect(new_levels, newf)
