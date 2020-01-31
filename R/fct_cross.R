4df7008699edd68c7489d6e11d0532506a684243
Hadley Wickham <h.wickham@gmail.com>
2020-01-31 15:32:41 -0600

    rlang is now imported

--- R/fct_cross.R
+++ R/fct_cross.R
@@ -25,11 +25,11 @@ fct_cross <- function(..., sep = ":", keep_empty = FALSE) {
   .data <- tibble::as_tibble(flist, .name_repair = "minimal")
   .data <- lapply(.data, check_factor)
 
-  newf <- rlang::exec(paste, !!! .data, sep = sep)
+  newf <- exec(paste, !!!.data, sep = sep)
 
   old_levels <- lapply(.data, levels)
-  grid <- rlang::exec(expand.grid, old_levels)
-  new_levels <- rlang::exec(paste, !!! grid, sep = sep)
+  grid <- exec(expand.grid, old_levels)
+  new_levels <- exec(paste, !!!grid, sep = sep)
 
   if (!keep_empty) {
     new_levels <- intersect(new_levels, newf)
