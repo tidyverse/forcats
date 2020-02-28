d2e05f5276b6edf2a80b07f6c8170cadb81bbd29
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2020-02-28 18:37:26 +0000

    Add `other_level` argument to `fct_collapse()` (#208)
    Fixes #194.

--- tests/testthat/test-collapse.R
+++ tests/testthat/test-collapse.R
@@ -23,21 +23,21 @@ test_that("can collapse missing levels", {
 
 test_that("can collapse un-named levels to Other", {
   f1 <- factor(letters[1:3])
-  f2 <- fct_collapse(f1, xy = c("a", "b"), group_other = TRUE)
+  f2 <- fct_collapse(f1, xy = c("a", "b"), other_level = "Other")
 
   expect_equal(f2, factor(c("xy", "xy", "Other"), levels = c("xy", "Other")))
 })
 
 test_that("collapses levels correctly when group_other is TRUE but no other variables to group", {
   f1 <- factor(letters[1:4])
-  f2 <- expect_warning(fct_collapse(f1, x1 = c("a", "b", "d"), x2 = "c", group_other = TRUE), "Unknown levels")
+  f2 <- fct_collapse(f1, x1 = c("a", "b", "d"), x2 = "c", other_level = "Other")
 
   expect_equal(f2, factor(c("x1", "x1", "x2", "x1"), levels = c("x1", "x2")))
 })
 
 test_that("collapses levels correctly when group_other is TRUE and some Other variables to group", {
   f1 <- factor(letters[1:4])
-  f2 <- fct_collapse(f1, x1 = c("a", "d"), x2 = "c", group_other = TRUE)
+  f2 <- fct_collapse(f1, x1 = c("a", "d"), x2 = "c", other_level = "Other")
 
   expect_equal(f2, factor(c("x1", "Other", "x2", "x1"), levels = c("x1", "x2", "Other")))
 })
@@ -47,4 +47,14 @@ test_that("does not automatically collapse unnamed levels to Other", {
   f2 <- fct_collapse(f1, xy = c("a", "b"))
 
   expect_equal(f2, factor(c("xy", "xy", "c"), levels = c("xy", "c")))
+})
+
+test_that("group_other is deprecated", {
+  f1 <- factor(letters[1:4])
+  f2 <- expect_warning(
+    fct_collapse(f1, x1 = c("a", "d"), x2 = "c", group_other = TRUE),
+    "deprecated"
+  )
+
+  expect_equal(levels(f2), c("x1", "x2", "Other"))
 })
