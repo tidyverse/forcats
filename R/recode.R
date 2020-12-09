23fd8af38b6bd767f2db213808def6888da62a5d
Hadley Wickham <h.wickham@gmail.com>
2020-12-09 08:27:03 -0600

    Tweak fct_recode docs
    Closes #272

--- R/recode.R
+++ R/recode.R
@@ -1,10 +1,10 @@
 #' Change factor levels by hand
 #'
 #' @param .f A factor (or character vector).
-#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> A sequence of named character vectors where the
-#'   name gives the new level, and the value gives the old level.
-#'   Levels not otherwise mentioned will be left as is. Levels can
-#'   be removed by naming them `NULL`. Uses tidy dots.
+#' @param ... <[`dynamic-dots`][rlang::dyn-dots]> A sequence of named character
+#'   vectors where the name gives the new level, and the value gives the old
+#'   level. Levels not otherwise mentioned will be left as is. Levels can
+#'   be removed by naming them `NULL`.
 #' @export
 #' @examples
 #' x <- factor(c("apple", "bear", "banana", "dear"))
@@ -15,6 +15,9 @@
 #'
 #' # If you name the level NULL it will be removed
 #' fct_recode(x, NULL = "apple", fruit = "banana")
+#'
+#' # Wrap the left hand side in quotes if it contains special variables
+#' fct_recode(x, "an apple" = "apple", "a bear" = "bear")
 #'
 #' # When passing a named vector to rename levels use !!! to splice
 #' x <- factor(c("apple", "bear", "banana", "dear"))
